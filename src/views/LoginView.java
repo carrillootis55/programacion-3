5f3525af2f0c5abcc31b733d577c25f900123fad
Eliizabetth <dmedinam2108@icloud.com>
2026-02-13 19:19:45 -0700

    FrontEnd: Ajustes en LoginView

--- src/views/LoginView.java
+++ src/views/LoginView.java
@@ -16,7 +16,7 @@ public class LoginView extends JPanel {
 	
 	public LoginView() {
 		
-		fuente = new Font("Arial", Font.ITALIC, 20);
+		fuente = new Font("Times New Roman", Font.ITALIC , 17);
 		setBackground(Color.WHITE);
 		setLayout(null);
 		
@@ -34,7 +34,7 @@ public class LoginView extends JPanel {
 		JButton boton = new JButton("Iniciar");
 		boton.setBounds(215,315,160,40);
 		boton.setBackground(Color.BLUE);
-		boton.setForeground(Color.WHITE);
+		boton.setForeground(Color.BLACK);
 		boton.setToolTipText("Haz click aquí");
 		boton.setFont(fuente);
 		
@@ -45,54 +45,65 @@ public class LoginView extends JPanel {
 	}
 	
 	private void crearFormulario() {
-		JLabel lblSaludo = new JLabel("¡Bienvenido! Inicie Sesion");
+		JLabel lblSaludo = new JLabel("Inicio de sesion");
+		
 		lblSaludo.setForeground(Color.BLACK);
 		lblSaludo.setFont(fuente);
-		lblSaludo.setBounds(10,0,400,40);
+		lblSaludo.setBounds(140,2,400,70);
 		add(lblSaludo);
 		
-		JLabel lblEmail = new JLabel("Correo: ");
+		JLabel lblEmail = new JLabel("*Correo: ");
 		lblEmail.setForeground(Color.BLACK);
 		lblEmail.setFont(fuente);
 		lblEmail.setBounds(10,100,200,40);
 		add(lblEmail);
 		
 		JTextField texto = new JTextField();
-		texto.setForeground(Color.BLUE);
+		texto.setForeground(Color.BLACK);
 		texto.setFont(fuente);
 		texto.setBounds(150,100,200,40);
 		add(texto);
 		
-		JLabel lblEmailCorreccion = new JLabel ("Ingrese el correo correctamente");
+		JLabel lblEmailCorreccion = new JLabel ("Correo obligatorio");
 		lblEmailCorreccion.setForeground(Color.RED);
 		lblEmailCorreccion.setFont(fuente);
-		lblEmailCorreccion.setBounds(200,120,220,50);
+		lblEmailCorreccion.setBounds(155,125,220,50);
+		
+		lblEmailCorreccion.setFont(
+				lblEmailCorreccion.getFont().deriveFont(14f)	
+				);
+		
 		//lblEmailCorreccion.setBounds(150,100,200,40);
 		add(lblEmailCorreccion);
 		
-		JLabel lblContrasena = new JLabel("Contraseña: ");
+		JLabel lblContrasena = new JLabel("*Contraseña: ");
 		lblContrasena.setForeground(Color.BLACK);
 		lblContrasena.setFont(fuente);
-		lblContrasena.setBounds(10,150,200,40);
+		lblContrasena.setBounds(10,160,200,40);
 		add(lblContrasena);
 		
 		JPasswordField contrasena = new JPasswordField();
 		contrasena.setForeground(Color.BLACK);
 		contrasena.setFont(fuente);
-		contrasena.setBounds(150,150,200,40);
+		contrasena.setBounds(150,160,200,40);
 		add(contrasena);
 		
-		JLabel lblContrasenaCorreccion = new JLabel ("Contraseña Incorrecta");
+		JLabel lblContrasenaCorreccion = new JLabel ("Contraseña obligatoria");
 		lblContrasenaCorreccion.setForeground(Color.RED);
 		lblContrasenaCorreccion.setFont(fuente);
-		lblContrasenaCorreccion.setBounds(200,170,220,60);
+		lblContrasenaCorreccion.setBounds(155,185,220,50);
+		
+		lblContrasenaCorreccion.setFont(
+				lblEmailCorreccion.getFont().deriveFont(14f)	
+				);
+		
 		add(lblContrasenaCorreccion);
 	}
 	
 	private void colocarIcono(JButton boton, String ruta) { 
 		try {
 			Image icono = ImageIO.read(getClass().getResource(ruta));
-			icono = icono.getScaledInstance(30, 30, Image.SCALE_SMOOTH);
+			icono = icono.getScaledInstance(20, 20, Image.SCALE_SMOOTH);
 			boton.setIcon(new ImageIcon(icono));			
 		}catch(Exception ex) {
 			System.out.println("No está la imagen del ícono");
