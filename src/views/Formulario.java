864a8908bd458915b3bae533d02cdad0bfadfc99
Otilio Gamboa <carrillootis55@gmail.com>
2026-02-25 09:42:12 -0700

    Se agrego mainView y se edito formulario

--- src/views/Formulario.java
+++ src/views/Formulario.java
@@ -7,10 +7,13 @@ import java.awt.Toolkit;
 
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
@@ -18,7 +21,7 @@ import javax.swing.SwingConstants;
 public class Formulario extends JFrame{
 	
 	public Formulario() {
-		setSize(300, 500); //lo reemplazo por pack
+		setSize(500, 500); //lo reemplazo por pack
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLocation(100,100);
@@ -38,23 +41,33 @@ public class Formulario extends JFrame{
 	
 	public void inicializarComponentes() {
 		
-		JLabel lblTitulo = new JLabel("GRUPO A");
+		JLabel lblTitulo = new JLabel("REGISTRO DE ALUMNO");
 		add(lblTitulo, BorderLayout.NORTH);
 		lblTitulo.setHorizontalAlignment(SwingConstants.CENTER);
-		
-		
-	
-		
-		
+
+
 		JPanel panelComponentes = new JPanel();
 		panelComponentes.setLayout(new BoxLayout(panelComponentes, BoxLayout.Y_AXIS));
 		panelComponentes.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
+		
+		//JCheckBox chkAceptoCondiciones = new JCheckBox("", true);
+		//(chkAceptoCondiciones);
+		
+		JRadioButton rbMujer = new JRadioButton("Mujer");
+		add(rbMujer);
+		JRadioButton rbHombre = new JRadioButton("Hombre");
+		add(rbHombre);
+		
+		ButtonGroup bgSexo = new ButtonGroup();
+		bgSexo.add(rbHombre);
+		bgSexo.add(rbMujer);
 		
 		JLabel matricula= new JLabel("Matricula:");
 		panelComponentes.add(matricula);
 		JTextField txt1 = new JTextField(10);
 		panelComponentes.add(txt1);
 		
+		//AGREGAR TURNO, FECHA DE NACIMIENTO, GENERO, DEJAR POR DEFECTO TURNO MATUTINO
 		
 		JLabel apellidoPaterno= new JLabel("Apellido Paterno:");
 		panelComponentes.add(apellidoPaterno);
@@ -71,24 +84,27 @@ public class Formulario extends JFrame{
 		JTextField txt = new JTextField(10);
 		panelComponentes.add(txt);
 		
-		JLabel contacto= new JLabel("Contacto de Emergencia:");
+		JLabel grupo= new JLabel("Grupo:");//OPCION
+		panelComponentes.add(grupo);
+		JTextField txt6 = new JTextField(10);
+		panelComponentes.add(txt6);
+		
+		
+		JLabel nombreContacto= new JLabel("Nombre del Contacto de Emergencia:");
+		panelComponentes.add(nombreContacto);
+		JTextField txt5 = new JTextField(10);
+		panelComponentes.add(txt5);
+		
+		JLabel contacto= new JLabel("Numero del Contacto de Emergencia:");
 		panelComponentes.add(contacto);
 		JTextField txt4 = new JTextField(10);
 		panelComponentes.add(txt4);
 		
-		
-		
-		
-		
-		
-		
-		
-		/*for(int i = 1; i < 10; i++) {
-			JLabel lbl = new JLabel("Nombre " + i);
-			panelComponentes.add(lbl);
-			JTextField txt = new JTextField(10);
-			panelComponentes.add(txt);
-		}*/
+		JLabel direccion= new JLabel("Domicilio (calle y numero):");
+		panelComponentes.add(direccion);
+		JTextField txt7 = new JTextField(10);
+		panelComponentes.add(txt7);
+		  
 		JScrollPane scroll = new JScrollPane(panelComponentes);
 		scroll.setHorizontalScrollBar(null);
 		
@@ -97,10 +113,9 @@ public class Formulario extends JFrame{
 	}
 	
 	private void crearBotones() {
-
 	    JPanel panelBoton = new JPanel();
 	    panelBoton.setBackground(Color.WHITE);
-
+	    
 	    JButton boton = new JButton("REGISTRAR ALUMNO");
 	    boton.setBackground(Color.WHITE);
 	    boton.setForeground(Color.BLACK);
@@ -110,8 +125,6 @@ public class Formulario extends JFrame{
 	    add(panelBoton, BorderLayout.SOUTH);
 	}
 
-	
-	
 	
 	
 }
