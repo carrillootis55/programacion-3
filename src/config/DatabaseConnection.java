ded93a11b9aa5ff7c4962b1ec544e62c85a49cc0
Otilio Gamboa <carrillootis55@gmail.com>
2026-05-12 12:00:39 -0700

    Cambio de ubicacion y modificacion del .gitignore y db.properties se saco del src y modificacion del databaseconnection

--- src/config/DatabaseConnection.java
+++ src/config/DatabaseConnection.java
@@ -1,31 +1,36 @@
 package config;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DatabaseConnection {
-    
-    private static final String URL = "jdbc:mysql://localhost:3306/sistema_primaria";
-    private static final String USER = "root";
-    private static final String PASSWORD = "MEMD060821";
-    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
-    
     private static Connection connection = null;
 
     public static Connection getConnection() throws SQLException {
-        try {
-            if (connection == null || connection.isClosed()) {
-                Class.forName(DRIVER);
-                connection = DriverManager.getConnection(URL, USER, PASSWORD);
-                System.out.println("Conexión establecida con sistema_primaria");
+        if (connection == null || connection.isClosed()) {
+            Properties props = new Properties();
+            
+            try (FileInputStream fis = new FileInputStream("db.properties")) {
+                props.load(fis);
+                
+                String url = props.getProperty("db.url");
+                String user = props.getProperty("db.user");
+                String pass = props.getProperty("db.password");
+                String driver = props.getProperty("db.driver");
+
+                Class.forName(driver);
+                connection = DriverManager.getConnection(url, user, pass);
+                System.out.println("Conexión establecida exitosamente");
+                
+            } catch (IOException | ClassNotFoundException e) {
+                System.err.println("Error: No se pudo leer el archivo db.properties o no existe.");
+                e.printStackTrace();
+                throw new SQLException("Error de configuración local.");
             }
-        } catch (ClassNotFoundException e) {
-            System.err.println("Error: No se encontró el conector MySQL (JAR)");
-            e.printStackTrace();
-        } catch (SQLException e) {
-            System.err.println("Error: Credenciales incorrectas o servidor apagado");
-            throw e;
         }
         return connection;
     }
