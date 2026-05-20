476b930bad6649ae4902267d3799173b881af8f2
Otilio Gamboa <carrillootis55@gmail.com>
2026-05-20 09:03:06 -0700

    Cambios a funciones para adaptarse a la base de datos

--- src/repository/AlumnoRepository.java
+++ src/repository/AlumnoRepository.java
@@ -53,41 +53,55 @@ public class AlumnoRepository {
     }*/
     
     //Guardar json
+	
 	public void save(Alumno alumno) throws Exception {
-		String sql = """
-			    INSERT INTO alumnos(
-			        matricula,
-			        nombre,
-			        apellido_paterno,
-			        apellido_materno,
-			        sexo,
-			        anio,
-			        grupo,
-			        contacto_emergencia,
-			        numero_emergencia,
-			        parentesco,
-			        domicilio
-			    )
-			    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-			    """;
-	    Connection conn = DatabaseConnection.getConnection();
-	    PreparedStatement ps = conn.prepareStatement(sql);
+        String sql = """
+            INSERT INTO alumno(
+                matricula,
+                nombre,
+                apellido_paterno,
+                apellido_materno,
+                sexo,
+                anio_id,
+                grupo_id,
+                contacto_emergencia,
+                numero_emergencia,
+                parentesco,
+                domicilio
+            )
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+            """;
 
-	    ps.setString(1, alumno.getMatricula());
-	    ps.setString(2, alumno.getNombre());
-	    ps.setString(3, alumno.getApellidoPaterno());
-	    ps.setString(4, alumno.getApellidoMaterno());
-	    ps.setString(5, String.valueOf(alumno.getSexo()));
-	    ps.setString(6, alumno.getAnio());
-	    ps.setString(7, alumno.getGrupo());
-	    ps.setString(8, alumno.getContactoEmergencia());
-	    ps.setString(9, alumno.getNumeroEmergencia());
-	    ps.setString(10, alumno.getParentesco());
-	    ps.setString(11, alumno.getDomicilio());
-	    
-	    ps.executeUpdate();
-	    ps.close();
-	}
+        Connection conn = DatabaseConnection.getConnection();
+
+        PreparedStatement ps = conn.prepareStatement(sql);
+
+        ps.setString(1, alumno.getMatricula());
+
+        ps.setString(2, alumno.getNombre());
+
+        ps.setString(3, alumno.getApellidoPaterno());
+
+        ps.setString(4, alumno.getApellidoMaterno());
+
+        ps.setString(5, String.valueOf(alumno.getSexo()));
+
+        ps.setInt(6, Integer.parseInt(alumno.getAnio()));
+
+        ps.setInt(7, alumno.getGrupo().equals("A") ? 1 : 2);
+
+        ps.setString(8, alumno.getContactoEmergencia());
+
+        ps.setString(9, alumno.getNumeroEmergencia());
+
+        ps.setString(10, alumno.getParentesco());
+
+        ps.setString(11, alumno.getDomicilio());
+
+        ps.executeUpdate();
+
+        ps.close();
+    }
     	
     	/*List<Alumno> alumnos = getAlumnos();
         alumnos.add(alumno);
@@ -110,58 +124,69 @@ public class AlumnoRepository {
     }
 */
     
-	 public void updateAlumno(Alumno alumno) throws Exception {
+	public void updateAlumno(Alumno alumno) throws Exception {
 
-	        // ===== NUEVO =====
-	        String sql = """
-	            UPDATE alumnos
-	            SET
-	                nombre = ?,
-	                apellido_paterno = ?,
-	                apellido_materno = ?,
-	                sexo = ?,
-	                anio = ?,
-	                grupo = ?,
-	                contacto_emergencia = ?,
-	                numero_emergencia = ?,
-	                parentesco = ?,
-	                domicilio = ?
-	            WHERE matricula = ?
-	            """;
+        String sql = """
+            UPDATE alumno
+            SET
+                nombre = ?,
+                apellido_paterno = ?,
+                apellido_materno = ?,
+                sexo = ?,
+                anio_id = ?,
+                grupo_id = ?,
+                contacto_emergencia = ?,
+                numero_emergencia = ?,
+                parentesco = ?,
+                domicilio = ?
+            WHERE matricula = ?
+            """;
 
-	        Connection conn = DatabaseConnection.getConnection();
-	        PreparedStatement ps = conn.prepareStatement(sql);
+        Connection conn = DatabaseConnection.getConnection();
 
-	        ps.setString(1, alumno.getNombre());
-	        ps.setString(2, alumno.getApellidoPaterno());
-	        ps.setString(3, alumno.getApellidoMaterno());
-	        ps.setString(4, String.valueOf(alumno.getSexo()));
-	        ps.setString(5, alumno.getAnio());
-	        ps.setString(6, alumno.getGrupo());
-	        ps.setString(7, alumno.getContactoEmergencia());
-	        ps.setString(8, alumno.getNumeroEmergencia());
-	        ps.setString(9, alumno.getParentesco());
-	        ps.setString(10, alumno.getDomicilio());
-	        ps.setString(11, alumno.getMatricula());
+        PreparedStatement ps = conn.prepareStatement(sql);
 
-	        ps.executeUpdate();
-	        ps.close();
-	}
+        ps.setString(1, alumno.getNombre());
+
+        ps.setString(2, alumno.getApellidoPaterno());
+
+        ps.setString(3, alumno.getApellidoMaterno());
+
+        ps.setString(4, String.valueOf(alumno.getSexo()));
+
+        ps.setInt(5, Integer.parseInt(alumno.getAnio()));
+
+        ps.setInt(6, alumno.getGrupo().equals("A") ? 1 : 2);
+
+        ps.setString(7, alumno.getContactoEmergencia());
+
+        ps.setString(8, alumno.getNumeroEmergencia());
+
+        ps.setString(9, alumno.getParentesco());
+
+        ps.setString(10, alumno.getDomicilio());
+
+        ps.setString(11, alumno.getMatricula());
+
+        ps.executeUpdate();
+
+        ps.close();
+    }
 	 
     //Eliminar alumno
-    public void delete(String matricula) throws Exception {
-    	String sql = "DELETE FROM alumnos WHERE matricula=?";
-    	
-    	Connection conn = DatabaseConnection.getConnection();
+	public void delete(String matricula) throws Exception {
+
+        String sql = "DELETE FROM alumno WHERE matricula=?";
+
+        Connection conn = DatabaseConnection.getConnection();
+
         PreparedStatement ps = conn.prepareStatement(sql);
 
         ps.setString(1, matricula);
 
         ps.executeUpdate();
 
-        ps.close();    
-        
-        
+        ps.close();
     }
     
     //Se obtienen y almacenan los alumnos csv
@@ -183,78 +208,60 @@ public class AlumnoRepository {
     }*/
     
     //Se obtienen y almacenan los alumnos json
-    public List<Alumno> getAlumnos() {
-    	System.out.println("Conectado");
-    	List<Alumno> alumnos = new ArrayList<>();
-    	
-    	String sql = "SELECT * FROM alumnos";
-    	
-    	try(Connection conn = DatabaseConnection.getConnection();
-    			PreparedStatement ps = conn.prepareStatement(sql);
-    			ResultSet rs = ps.executeQuery()){
-    		
-    		while(rs.next()) {
-    			System.out.println(rs.getString("nombre"));
-    			Alumno alumno = new Alumno(
-	               rs.getString("matricula"),
-	               rs.getString("nombre"),
-	               rs.getString("apellido_paterno"),
-	               rs.getString("apellido_materno"),
-	               rs.getString("sexo").charAt(0),
-	               rs.getString("anio"),
-	               rs.getString("grupo"),
-	               rs.getString("contacto_emergencia"),
-	               rs.getString("numero_emergencia"),
-	               rs.getString("parentesco"),
-	               rs.getString("domicilio")
-	               
-    		);
-    		
-    		}
-    	
-    	}catch(Exception e) {
-    		e.printStackTrace();
-    	}
-    	
-     
-    	return alumnos;
-    	
-    
-    }
-    
-    public List<Alumno> getAlumnosPorGrupo(String anio, String grupo) {
+	public List<Alumno> getAlumnos() {
+
+        System.out.println("Conectado");
 
         List<Alumno> alumnos = new ArrayList<>();
 
         String sql = """
-                SELECT *
-                FROM alumnos
-                WHERE anio = ? AND grupo = ?
-                """;
+            SELECT
+                a.*,
+                an.tipo AS anio,
+                g.tipoGrupo AS grupo
+            FROM alumno a
+            INNER JOIN anio an
+                ON a.anio_id = an.id
+            INNER JOIN grupo g
+                ON a.grupo_id = g.id
+            """;
 
         try (
+
             Connection conn = DatabaseConnection.getConnection();
-            PreparedStatement ps = conn.prepareStatement(sql)
+
+            PreparedStatement ps = conn.prepareStatement(sql);
+
+            ResultSet rs = ps.executeQuery()
+
         ) {
-
-            ps.setString(1, anio);
-            ps.setString(2, grupo);
-
-            ResultSet rs = ps.executeQuery();
 
             while (rs.next()) {
 
+                System.out.println(rs.getString("nombre"));
+
                 Alumno alumno = new Alumno(
+
                     rs.getString("matricula"),
+
                     rs.getString("nombre"),
+
                     rs.getString("apellido_paterno"),
+
                     rs.getString("apellido_materno"),
+
                     rs.getString("sexo").charAt(0),
+
                     rs.getString("anio"),
+
                     rs.getString("grupo"),
+
                     rs.getString("contacto_emergencia"),
+
                     rs.getString("numero_emergencia"),
+
                     rs.getString("parentesco"),
+
                     rs.getString("domicilio")
                 );
 
@@ -262,11 +269,84 @@ public class AlumnoRepository {
             }
 
         } catch (Exception e) {
+
             e.printStackTrace();
         }
 
         return alumnos;
     }
+
+  
+	 public List<Alumno> getAlumnosPorGrupo(String anio, String grupo) {
+
+	        List<Alumno> alumnos = new ArrayList<>();
+
+	        String sql = """
+	            SELECT
+	                a.*,
+	                an.tipo AS anio,
+	                g.tipoGrupo AS grupo
+	            FROM alumno a
+	            INNER JOIN anio an
+	                ON a.anio_id = an.id
+	            INNER JOIN grupo g
+	                ON a.grupo_id = g.id
+	            WHERE an.tipo = ?
+	            AND g.tipoGrupo = ?
+	            """;
+
+	        try (
+
+	            Connection conn = DatabaseConnection.getConnection();
+
+	            PreparedStatement ps = conn.prepareStatement(sql)
+
+	        ) {
+
+	            ps.setString(1, anio);
+
+	            ps.setString(2, grupo);
+
+	            ResultSet rs = ps.executeQuery();
+
+	            while (rs.next()) {
+
+	                Alumno alumno = new Alumno(
+
+	                    rs.getString("matricula"),
+
+	                    rs.getString("nombre"),
+
+	                    rs.getString("apellido_paterno"),
+
+	                    rs.getString("apellido_materno"),
+
+	                    rs.getString("sexo").charAt(0),
+
+	                    rs.getString("anio"),
+
+	                    rs.getString("grupo"),
+
+	                    rs.getString("contacto_emergencia"),
+
+	                    rs.getString("numero_emergencia"),
+
+	                    rs.getString("parentesco"),
+
+	                    rs.getString("domicilio")
+	                );
+
+	                alumnos.add(alumno);
+	            }
+
+	        } catch (Exception e) {
+
+	            e.printStackTrace();
+	        }
+
+	        return alumnos;
+	    }
+    
   
     
     
