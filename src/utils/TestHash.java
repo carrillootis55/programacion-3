843a0909f244a4ed10b07b55ac010ac212e8c2c8
Eliizabethh <dmedinam2108@icloud.com>
2026-05-21 07:53:56 -0700

    testhash

--- src/utils/TestHash.java
+++ src/utils/TestHash.java
@@ -2,7 +2,7 @@ package utils;
 
 public class TestHash {
     public static void main(String[] args) {
-        String hash = PasswordUtils.hashPassword("1234");
+        String hash = PasswordUtils.hashPassword("2127");
         System.out.println(hash);
     }
 }
