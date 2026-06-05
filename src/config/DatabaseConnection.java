c5355caebc552f4a159e0212ba03620097e14981
Eliizabethh <dmedinam2108@icloud.com>
2026-06-05 13:48:23 -0700

    ..

--- src/config/DatabaseConnection.java
+++ src/config/DatabaseConnection.java
@@ -12,8 +12,7 @@ public class DatabaseConnection {
 
     /*DriverManager es una clase, su trabajo es trabajar como el "jefe" y coordinador de todas las conexiones de bd
      *En lugar de escribir la contrasena directamente en el codigo usamos un archivo de texto 
-     *db.properties que lee cuatro variables la dirección (url), el usuario (user), la contraseña (pass) y el nombre del driver (driver)
-     */
+     *db.properties que lee cuatro variables la dirección (url), el usuario (user), la contraseña (pass) y el nombre del driver (driver)*/
     public static Connection getConnection() throws SQLException {
         if (connection == null || connection.isClosed()) {
             Properties props = new Properties();
@@ -25,7 +24,7 @@ public class DatabaseConnection {
                 String user = props.getProperty("db.user");
                 String pass = props.getProperty("db.password");
                 String driver = props.getProperty("db.driver");
-                //AQUI carga la memoria ram en el driver basicamente le dice a java el driver despierta porque hablaremos mysql
+                //AQUI carga la memoria ram en el driver basicamente le dice java al driver despierta porque hablaremos mysql
                 Class.forName(driver);
                 
                 connection = DriverManager.getConnection(url, user, pass);
